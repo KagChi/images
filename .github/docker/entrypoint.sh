31631af133c110d03a0e9ffd7104ab6bb8992420
KagChi <59391215+KagChi@users.noreply.github.com>
2024-08-08 16:18:13 +0000

    feat: add image

---
+++ .github/docker/entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/ash -e
+
+#mkdir -p /var/log/supervisord/ /var/log/php8/ \
+
+## check for .env file and generate app keys if missing
+if [ -f /pelican-data/.env ]; then
+  echo "external vars exist."
+  rm -rf /var/www/html/.env
+else
+  echo "external vars don't exist."
+  rm -rf /var/www/html/.env
+  touch /pelican-data/.env
+
+  ## manually generate a key because key generate --force fails
+  if [ -z $APP_KEY ]; then
+     echo -e "Generating key."
+     APP_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+     echo -e "Generated app key: $APP_KEY"
+     echo -e "APP_KEY=$APP_KEY" > /pelican-data/.env
+  else
+    echo -e "APP_KEY exists in environment, using that."
+    echo -e "APP_KEY=$APP_KEY" > /pelican-data/.env
+  fi
+fi
+
+ln -s /pelican-data/.env /var/www/html/
+
+touch /pelican-data/database.sqlite
+
+if ! grep -q "APP_KEY=" .env || grep -q "APP_KEY=$" .env; then
+  echo "Generating APP_KEY..."
+  php artisan key:generate --force
+else
+  echo "APP_KEY is already set."
+fi
+
+## make sure the db is set up
+echo -e "Migrating and Seeding Database"
+php artisan migrate --force
+
+## start cronjobs for the queue
+echo -e "Starting cron jobs."
+crond -L /var/log/crond -l 5
+
+if [[ -z $SKIP_CADDY ]]; then
+  echo "Starting PHP-FPM and Caddy"
+  caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+else
+  echo "Starting PHP-FPM only"
+fi
+
+#chmod -R 755 storage/* bootstrap/cache/
+chown -R www-data:www-data .
+
+echo -e "Starting supervisord."
+exec "$@"
---
+++ Dockerfile
@@ -0,0 +1,56 @@
+# Pelican Production Dockerfile
+
+FROM node:20-alpine AS yarn
+
+WORKDIR /build
+
+COPY . ./
+
+RUN yarn install --frozen-lockfile && yarn run build:production
+
+FROM php:8.3-fpm-alpine
+
+COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
+
+WORKDIR /var/www/html
+
+# Install dependencies
+RUN apk update && apk add --no-cache \
+    libpng-dev libjpeg-turbo-dev freetype-dev libzip-dev icu-dev git \
+    zip unzip curl \
+    caddy ca-certificates supervisor \
+    && docker-php-ext-install bcmath gd intl zip opcache pcntl posix
+
+RUN git clone https://github.com/pelican-dev/panel && cd panel
+
+# Copy the Caddyfile to the container
+COPY ../Caddyfile /etc/caddy/Caddyfile
+
+# Copy the application code to the container
+COPY . .
+
+COPY --from=yarn /build/public/assets ./public/assets
+
+RUN cp .env.docker .env
+
+RUN composer install --no-dev --optimize-autoloader
+
+# Set file permissions
+RUN chmod -R 755 /var/www/html/storage \
+    && chmod -R 755 /var/www/html/bootstrap/cache
+
+#echo "* * * * * /usr/local/bin/php /build/artisan schedule:run >> /dev/null 2>&1" >> /var/spool/cron/crontabs/root
+
+HEALTHCHECK --interval=5m --timeout=10s --start-period=5s --retries=3 \
+  CMD curl -f http://localhost/up || exit 1
+
+EXPOSE 80:2019
+EXPOSE 443
+
+VOLUME /pelican-data
+
+# Start PHP-FPM
+CMD ["sh", "-c", "php-fpm"]
+
+ENTRYPOINT [ "/bin/ash", ".github/docker/entrypoint.sh" ]
+# CMD [ "supervisord", "-n", "-c", "/etc/supervisord.conf" ]
